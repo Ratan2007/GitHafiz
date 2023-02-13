8e347337791c391bf069d530b1d559475eab2749
Ratan2007 <skratan2007@gmail.com>
2023-02-13 07:49:10 -0500

    delete method

--- src/test/java/hafizgitPractice/HafizGitPractice1.java
+++ src/test/java/hafizgitPractice/HafizGitPractice1.java
@@ -7,13 +7,6 @@ public class HafizGitPractice1 {
 	public void add1() {
 		System.out.println("This is add 1");
 	}
-	@Test
-	public void add2() {
-		System.out.println("This is add 2");
-	}
-	@Test
-	public void add3() {
-		System.out.println("This is add 3");
-	}
+
 	
 }
